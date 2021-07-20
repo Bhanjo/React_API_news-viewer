dcdef1af4ea04b34ea1aa298d8443baaa655f906
Bhanjo <hanjo1515@naver.com>
2021-07-20 14:29:11 +0900

    추가: API를 통해 뉴스 불러오기 기능 구현

--- news_viewer/src/App.js
+++ news_viewer/src/App.js
@@ -2,7 +2,26 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  
-}
+  const [data, setData] = useState(null);
+  // asycn await 사용
+  const onClick = async () => {
+    try{
+      const response = await axios.get(
+        'https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=c0662f2484fb4c458135523c837f41d5'
+      );
+      setData(response.data);
+    } catch(e) {
+      console.log(e);
+    }
+  };
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
+    </div>
+  );
+};
 
 export default App;
