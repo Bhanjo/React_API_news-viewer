5650a472e1cce67241a3f0855b1d7d6310a54770
Bhanjo <hanjo1515@naver.com>
2021-07-21 15:48:31 +0900

    리팩토링: 커스텀 hook을 통한 상태관리

--- news_viewer/src/components/NewsList.js
+++ news_viewer/src/components/NewsList.js
@@ -1,8 +1,9 @@
 // 뉴스 데이터 배열 렌더링
-import React, { useState, useEffect} from "react";
+import React from "react";
 import styled from 'styled-components';
 import NewsItem from "./NewsItem";
 import axios from "axios";
+import usePromise from "../lib/usePromise";
 
 // 스타일 컴포넌트
 const NewsListBlock = styled.div`
@@ -19,37 +20,31 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-    const [articles, setArticles] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(() => {
-        // async 사용하는 함수 따로 선언
-        const fetchData = async () => {
-            setLoading(true);
-            try {
-                // 쿼리의 카테고리가 all이면 그대로, 아니면 `&category=카테고리`를 주소에 넣음
-                const query = category === 'all' ? '' : `&category=${category}`;
-                const response = await axios.get(
-                    `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c0662f2484fb4c458135523c837f41d5`,
-                );
-                setArticles(response.data.articles);
-            } catch(e) {
-                console.log(e);
-            }
-            setLoading(false);
-        };
-        fetchData();
-    },[category]); // 카테고리가 바뀔 때마다 뉴스를 새로 불러와야함
+    // usePromise 커스텀 Hook 사용
+    const [loading, response, error] = usePromise(() => {
+        const query = category === 'all' ? '' : `&category=${category}`;
+        return axios.get(
+            `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=c0662f2484fb4c458135523c837f41d5`,
+        );
+    }, [category]);
 
     // 대기 중일 때
     if(loading) {
         return <NewsListBlock>대기중...</NewsListBlock>
     }
-    // article 값이 설정되지 않았을 때
-    if(!articles) {
+
+    // response 값이 설정되지 않았을 때
+    if(!response) {
         return null;
     }
 
+    // 에러가 발생
+    if(error) {
+        return <NewsListBlock>에러가 발생했습니다!</NewsListBlock>
+    }
+
+    // response 값이 유효할 때
+    const { articles } = response.data;
     return (
         <NewsListBlock>
             {/* map 함수를 통해 item 추가 */}
